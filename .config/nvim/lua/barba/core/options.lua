aeb9a1e002dc1d2dfbb1e4603cd2edd437c9196b
abbru <abbrumax@gmail.com>
2026-01-29 19:08:26 +0000

    fix wrap in nvim

--- .config/nvim/lua/barba/core/options.lua
+++ .config/nvim/lua/barba/core/options.lua
@@ -11,7 +11,7 @@ opt.shiftwidth = 4 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
-opt.wrap = false
+opt.wrap = true
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
